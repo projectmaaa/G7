756cb46662f895a31c655298e250df7ce91b81b1
Alex.Dr <Alex.Dru2013@gmail.com>
2018-06-15 17:13:02 +0300

    when the timer stops and submission locked and submitted to the server.

--- G7_Prototype_Client/src/resources/ReportAboutTeacher.java
+++ G7_Prototype_Client/src/resources/ReportAboutTeacher.java
@@ -10,8 +10,8 @@ public class ReportAboutTeacher extends Report implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private String teacherName;
-	
+
 	public ReportAboutTeacher() {
-		
+
 	}
 }
